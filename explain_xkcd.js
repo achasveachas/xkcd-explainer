64013796f5528b707bcb4d532243ec6cdee6d93b
Yechiel Kalmenson <contact@yechiel.me>
2018-10-06 22:15:26 -0400

    create the add-on

---
+++ explain_xkcd.js
@@ -0,0 +1,22 @@
+function addExplainButton() {
+    const explainURL = "https://www.explainxkcd.com" + window.location.pathname
+    const newButton = document.createElement("li")
+    buttonContent = document.createElement("a")
+    buttonContent.href = explainURL
+    buttonContent.innerHTML = "Huh?"
+    newButton.appendChild(buttonContent)
+
+    // The desktop version has 2 navbars with class .comicNav and the mobile version has one navbar with the id #navButtons
+    let navbars = document.getElementsByClassName("comicNav");
+    if (navbars.length === 0) {
+        navbars = [document.getElementById("navButtons")];
+    }
+
+    for (let i = 0; i < navbars.length; i++) {
+        const navButtons = Array.from(navbars[i].children);
+        const randomButton = navButtons.find((li => li.firstChild.textContent === "Random" || li.firstChild.textContent === "?"))
+        navbars[i].insertBefore(newButton.cloneNode(true), randomButton.nextElementSibling);
+    }
+}
+
+addExplainButton();
